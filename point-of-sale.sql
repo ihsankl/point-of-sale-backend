462a9d42a9cb9461a1bf76d2f7d602923844b887
Ihsan Kurniawan <ikurniawanlubis@gmail.com>
2021-12-30 11:02:41 +0700

    Create point-of-sale.sql

---
+++ point-of-sale.sql
@@ -0,0 +1,381 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Dec 30, 2021 at 04:02 AM
+-- Server version: 10.6.5-MariaDB-1:10.6.5+maria~focal
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `point-of-sale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Customer`
+--
+
+CREATE TABLE `Customer` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `contact` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Invoice`
+--
+
+CREATE TABLE `Invoice` (
+  `id` int(11) NOT NULL,
+  `total_amount` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `amount_tendered` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `date_recorded` date NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `unit_in_stock` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `disc_percentage` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `unit_price` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `re_order_level` varchar(255) DEFAULT NULL,
+  `unit_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product Category`
+--
+
+CREATE TABLE `Product Category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '-'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product Unit`
+--
+
+CREATE TABLE `Product Unit` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Purchase Order`
+--
+
+CREATE TABLE `Purchase Order` (
+  `id` int(11) NOT NULL,
+  `qty` int(11) UNSIGNED NOT NULL,
+  `sub_total` int(11) UNSIGNED NOT NULL,
+  `order_date` date NOT NULL DEFAULT current_timestamp(),
+  `unit_price` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Receive Product`
+--
+
+CREATE TABLE `Receive Product` (
+  `id` int(11) NOT NULL,
+  `qty` int(11) UNSIGNED NOT NULL,
+  `unit_price` int(11) UNSIGNED NOT NULL,
+  `sub_total` int(11) UNSIGNED NOT NULL,
+  `received_date` date NOT NULL DEFAULT current_timestamp(),
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Revoked Tokens`
+--
+
+CREATE TABLE `Revoked Tokens` (
+  `id` int(11) NOT NULL,
+  `token` text NOT NULL,
+  `signed_out` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Sales`
+--
+
+CREATE TABLE `Sales` (
+  `id` int(11) NOT NULL,
+  `qty` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `unit_price` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `sub_total` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `invoice_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Supplier`
+--
+
+CREATE TABLE `Supplier` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `role` varchar(255) NOT NULL,
+  `contact` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Customer`
+--
+ALTER TABLE `Customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Invoice`
+--
+ALTER TABLE `Invoice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category_id`),
+  ADD KEY `user` (`user_id`),
+  ADD KEY `unit` (`unit_id`);
+
+--
+-- Indexes for table `Product Category`
+--
+ALTER TABLE `Product Category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Product Unit`
+--
+ALTER TABLE `Product Unit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Purchase Order`
+--
+ALTER TABLE `Purchase Order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Receive Product`
+--
+ALTER TABLE `Receive Product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Revoked Tokens`
+--
+ALTER TABLE `Revoked Tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Sales`
+--
+ALTER TABLE `Sales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `invoice_id` (`invoice_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `Supplier`
+--
+ALTER TABLE `Supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Customer`
+--
+ALTER TABLE `Customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Invoice`
+--
+ALTER TABLE `Invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Product Category`
+--
+ALTER TABLE `Product Category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Product Unit`
+--
+ALTER TABLE `Product Unit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Purchase Order`
+--
+ALTER TABLE `Purchase Order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Receive Product`
+--
+ALTER TABLE `Receive Product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Revoked Tokens`
+--
+ALTER TABLE `Revoked Tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Sales`
+--
+ALTER TABLE `Sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Supplier`
+--
+ALTER TABLE `Supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Invoice`
+--
+ALTER TABLE `Invoice`
+  ADD CONSTRAINT `Invoice_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Product`
+--
+ALTER TABLE `Product`
+  ADD CONSTRAINT `category` FOREIGN KEY (`category_id`) REFERENCES `Product Category` (`id`),
+  ADD CONSTRAINT `unit` FOREIGN KEY (`unit_id`) REFERENCES `Product Unit` (`id`),
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Purchase Order`
+--
+ALTER TABLE `Purchase Order`
+  ADD CONSTRAINT `Purchase Order_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`),
+  ADD CONSTRAINT `Purchase Order_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `Supplier` (`id`),
+  ADD CONSTRAINT `Purchase Order_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Receive Product`
+--
+ALTER TABLE `Receive Product`
+  ADD CONSTRAINT `Receive Product_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`),
+  ADD CONSTRAINT `Receive Product_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `Supplier` (`id`),
+  ADD CONSTRAINT `Receive Product_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Sales`
+--
+ALTER TABLE `Sales`
+  ADD CONSTRAINT `Sales_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `Invoice` (`id`),
+  ADD CONSTRAINT `Sales_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
