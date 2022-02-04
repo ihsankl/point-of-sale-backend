fa9d6c73f786685e5b766b4c9fccd9e5d9f2ad76
Ihsan Kurniawan <ikurniawanlubis@gmail.com>
2022-02-04 19:07:03 +0700

    Update receive_product.js

--- routes/receive_product.js
+++ routes/receive_product.js
@@ -48,8 +48,8 @@ router.get('/pagination', auth, async (req, res, next) => {
   const {page, limit, date_from, date_to} = req.query;
   const query = knex
       .select(
-          'b.name as product_name',
           'a.*',
+          'b.name as product_name',
           'c.name as supplier_name',
       )
       .from('Receive Product as a')
@@ -75,8 +75,8 @@ router.get('/pagination', auth, async (req, res, next) => {
 router.get('/', auth, async (req, res, next) => {
   const query = knex
       .select(
-          'a.*',
           'b.name as product_name',
+          'a.*',
           'c.name as supplier_name',
       )
       .from('Receive Product as a')
